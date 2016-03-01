80ebab614628a4e23a4f4dd162b56fb0249ebe81
Jorge Capona <jcapona@gmail.com>
2016-03-01 16:10:16 -0300

    Returns object with 'plottable' data to frontend

--- controllers/StockController.js
+++ controllers/StockController.js
@@ -1,3 +1,4 @@
+var Stock = require('../models/Stock.js');
 var Quandl = require("quandl");
 var quandl = new Quandl({
   auth_token: process.env.QUANDL_KEY
@@ -18,8 +19,29 @@ exports.readOne = function (req, res) {
   search.table = req.params.id;
   quandl.dataset(search, options, function(err, response){
       if(err)
-          throw err;
-   
-      res.json(JSON.parse(response));// Must use JSON.parse! 
+        throw err;
+      
+      if(JSON.parse(response).dataset.data)
+      {
+        var dat = JSON.parse(response).dataset;
+        
+        var date=[], vals=[];
+        dat.data.forEach(function(val, index){
+          date.push(val[0]);
+          vals.push(val[1]);
+          
+          if(index + 1 === dat.data.length)
+          {
+            var obj = {};
+            obj._id = dat.id;
+            obj.name = dat.dataset_code;
+            obj.dates = date;
+            obj.values = vals;
+            //obj.backup = dat;
+
+            res.json(obj);
+          }
+        });
+      }
   });
 };
