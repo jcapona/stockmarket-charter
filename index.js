29eecb35c4eeda0c5bc669297a2721dfaf2f6b17
Jorge Capona <jcapona@gmail.com>
2016-03-02 02:31:09 -0300

    Removes unecessary dependencies and changes server port

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();  
 var server = require('http').Server(app);  
 var io = require('socket.io')(server);
-var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 
 require('dotenv').config();
@@ -10,8 +9,8 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + 'views'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {  
   res.sendFile(__dirname+'/views/index.html');
@@ -20,6 +19,4 @@ app.get('/', function(req, res) {
 var StockController = require('./controllers/StockController');
 io.on('connection', StockController.getData);
 
-server.listen(8080, function() {  
-  console.log("Server listening on port 8080");
-});
+server.listen(app.get('port'));
