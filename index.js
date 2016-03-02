4b0d1129fa69b94e08df2db6874301d5fa11c552
Jorge Capona <jcapona@gmail.com>
2016-03-01 22:08:08 -0300

    Uses express-ws to manage websocket connection

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var expressWs = require('express-ws')(app);
+var mongoose = require("mongoose");
+
 require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + 'views'));
@@ -9,7 +13,6 @@ app.use(express.static(__dirname + 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Routes 
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/views/index.html');
 });
@@ -17,7 +20,7 @@ app.get('/', function (req, res) {
 // CRUD operations
 var StockController = require('./controllers/StockController');
 app.get('/data/:id',StockController.readOne); // Read One
-
+expressWs.getWss().on('connection', StockController.getData);
 
 app.get('/*', function (req, res) {
   res.redirect('/');
